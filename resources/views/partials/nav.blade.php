ce66e1a03543d4fd7c943dcc1d8a0b2f05bf5894
Martin Pruus <martinprs@users.noreply.github.com>
2026-09-24 15:48:46 +0300

    Update navbar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -34,7 +34,7 @@
                     <summary>Admin</summary>
                     <ul class="p-2">
                         <li><a href="{{ route('posts.index') }}">Posts</a></li>
-                        <li><a>Submenu 2</a></li>
+                        <li><a href="{{ route('tags.index') }}">Tags</a></li>
                     </ul>
                 </details>
             </li>
